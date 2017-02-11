62f8b6ccdc79201b41008bc370e2eedec26b281f
Robokoprob <rob_dl@hotmail.com>
2017-02-11 14:29:29 +0100

    Creating webpack config in IIFE with env parameter
    Env parameter can be used to pass environment variables to optimize builds depending on dev/prod

--- ReactWebsite/webpack.config.js
+++ ReactWebsite/webpack.config.js
@@ -1,50 +1,51 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var config = {
-    entry: './src/main.js',
+var config = ((env = {}) => {
+    return {
+        entry: './src/main.js',
 
-    output: {
-        path:'./build',
-        filename: 'bundle.js',
-        publicPath: "/build"
-    },
+        output: {
+            path:'./build',
+            filename: 'bundle.js',
+            publicPath: "/build"
+        },
 
-    devServer: {
-        inline: true,
-        port: 8082
-    },
+        devServer: {
+            inline: true,
+            port: 8082
+        },
 
-    debug: true,
+        debug: true,
 
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel',
+        module: {
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    loader: 'babel',
 
-                query: {
-                    presets: ['es2015', 'react']
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    loader: 'style!css?modules&localIdentName=[name]__[local]__[hash:base64:5]'
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: /node_modules/,
+                    loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass')
                 }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                loader: 'style!css?modules&localIdentName=[name]__[local]__[hash:base64:5]'
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass')
-            }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("styles.css", {
+                allChunks: true
+            })
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin("styles.css", {
-            allChunks: true
-        })
-    ]
-};
-
+    };
+})()
 module.exports = config;
